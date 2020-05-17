4027892bbebd56c5101dead88032bc17de452df1
Stepan Kozurak <step.kozbvb@gmail.com>
2020-05-17 15:12:03 +0200

    D6.11 is done

--- my_site/my_site/urls.py
+++ my_site/my_site/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from p_library import views
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,5 +26,7 @@ urlpatterns = [
     path('index/book_increment/', views.book_increment),
     path('index/book_decrement/', views.book_decrement),
     path('publishes/', views.redactions),
-    path('', include('p_library.urls')),
+    path('', views.index_redirect),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
