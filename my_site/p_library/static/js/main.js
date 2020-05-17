675280173467f130659e40e541ead6a0f5a1b788
Stepan Kozurak <step.kozbvb@gmail.com>
2020-05-17 22:43:15 +0200

    Preloader added

--- my_site/p_library/static/js/main.js
+++ my_site/p_library/static/js/main.js
@@ -1,3 +1,21 @@
+function onReady(callback) {
+    const intervalId = window.setInterval(function () {
+        if (document.getElementsByTagName('body')[0] !== undefined) {
+            window.clearInterval(intervalId);
+            callback.call(this);
+        }
+    }, 1000);
+}
+
+function setVisible(selector, visible) {
+    document.querySelector(selector).style.display = visible ? 'flex' : 'none';
+}
+
+onReady(function () {
+    setVisible('main', true);
+    setVisible('.preloader-wrapper.big.active', false);
+});
+
 $(document).ready(function () {
     $('.sidenav').sidenav();
     $('.materialboxed').materialbox();
